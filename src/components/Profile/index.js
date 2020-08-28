c9b44bafc627500dba7f46c6daf19abef31a37cb
maira-o <mairasetecores@gmail.com>
2020-08-28 16:47:23 -0300

    creating profile styled.js

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import Avatar from '../Avatar'
+import * as S from './styled'
 
 
 const Profile = () => {
@@ -21,12 +22,21 @@ const Profile = () => {
     `)
 
     return (
-        <div className="Profile-wrapper">
+        <S.ProfileWrapper>
+
+          <S.ProfileLink>
             <Avatar />
-            <h1>{title}</h1>
-            <h2>{position}</h2>
-            <p>{description}</p>
-        </div>
+            <S.ProfileAuthor>
+              {title}
+              <S.ProfilePosition>
+                {position}
+              </S.ProfilePosition>
+            </S.ProfileAuthor>
+          </S.ProfileLink>
+
+            <S.ProfileDescription>{description}</S.ProfileDescription>
+
+        </S.ProfileWrapper>
     )
 }
 
