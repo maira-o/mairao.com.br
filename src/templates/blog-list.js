cf72d2c74ead5b3cffa0ba547b90a5a9f2337f12
maira-o <mairasetecores@gmail.com>
2020-08-31 13:40:41 -0300

    Create pagination with Gatsby Node API

---
+++ src/templates/blog-list.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+
+import Layout from "../components/Layout"
+import PostItem from "../components/PostItem"
+
+import SEO from "../components/seo"
+
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
+
+  return (
+    <Layout>
+      <SEO title="home" />
+      {postList.map(({ 
+        node: {
+          frontmatter: { background, category, date, description, title },
+          timeToRead,
+          fields: { slug }
+        },
+      }) => (
+        <PostItem 
+          slug={slug}
+          category={category}
+          background={background}
+          date={date}
+          timeToRead={timeToRead}
+          title={title}
+          description={description}
+        />
+      ))}
+    </Layout>
+  )
+
+}
+
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: {fields: frontmatter___date, order: DESC}
+      limit: $limit
+      skip: $skip
+      ) {
+      edges {
+        node {
+          fields {
+            slug
+          },
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          },
+          timeToRead,
+        }
+      }
+    }
+  }
+`
+
+export default BlogList
