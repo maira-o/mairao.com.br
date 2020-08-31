e6325c88c8e9a4ddff1a25f7b7489dc21241cd53
maira-o <mairasetecores@gmail.com>
2020-08-31 17:45:08 -0300

    Add bassis styled to a blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,14 +4,32 @@ import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import SEO from "../components/seo"
 
+import * as S from '../components/Post/styled'
+
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
 
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <h1> Title: {post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+
+        <S.PostTitle>
+          {post.frontmatter.title}
+        </S.PostTitle>
+
+        <S.PostDescription>
+          {post.frontmatter.description}
+        </S.PostDescription>
+      </S.PostHeader>
+      
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
     </Layout>
   )
 }
@@ -19,13 +37,13 @@ const BlogPost = ({ data }) => {
 export const query = graphql`
   query Post($slug: String!) {
     markdownRemark(fields: {slug: {eq: $slug}}) {
-      fields {
-        slug
-      }
       frontmatter {
         title 
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html 
+      timeToRead
     }
   }
 `
